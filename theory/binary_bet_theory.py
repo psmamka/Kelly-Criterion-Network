9b4a0829e9b9cab81f8e5b40696904f66cde4b68
P S Mamkani <psmamkani@protonmail.com>
2022-11-04 08:14:45 -0700

    Logarithmic performance

--- theory/binary_bet_theory.py
+++ theory/binary_bet_theory.py
@@ -16,43 +16,73 @@
 # 
 # For a n-step trial, with compounding of returns, we sum over all possible combinations of gain/loss sequences:
 # 
-# E(s | n-step) / s = Σ n!/k!/(n-k)! * win_pr^k * loss_pr^(n-k) * (1 + bet_fr * win_fr)^k * (1 - bet_fr * loss_fr)^(n-k)          
+# E(s | n-step) / s = Σ n!/k!/(n-k)!  *  win_pr^k * loss_pr^(n-k)  *  (1 + bet_fr * win_fr)^k * (1 - bet_fr * loss_fr)^(n-k)          
+#                       ------------    --------------------------    ------------------------------------------------------
+#                       ↑ num combs ↑   ↑ prob. of a combination ↑       ↑  return of the k-win, (n-k)-loss combination  ↑
+# 
 # with summation over k = 0 to n, each k corresponding to k wins and n-k losses, and
 # n!/k!/(n-k)! corresponding to number of possibilities of k wins in a sequence of n trades.
-
+# 
+# When optimizing the logarithm of returns, the fomula is updated to:
+# 
+# E(log(s) | n-step) - log(s) = Σ n!/k!/(n-k)! * win_pr^k * loss_pr^(n-k) * log[ (1 + bet_fr * win_fr)^k * (1 - bet_fr * loss_fr)^(n-k) ]
+# 
+# 
 
 import numpy as np
 from scipy.special import comb
 
-def bet_n_times_outcome(n, bet_fr, win_pr, win_fr=1.0, loss_fr=1.0):
+def bet_n_times_outcome(n, bet_fr, win_pr, win_fr=1.0, loss_fr=1.0, log_return=False):
     if type(bet_fr) is np.ndarray:
         outcomes = np.zeros(bet_fr.shape)
     else:
         outcomes = 0
     
-    for k in range(n + 1):
-        outcomes += comb(n, k, exact=True) * win_pr**k * (1 - win_pr)**(n-k) * \
-                        (1 + bet_fr * win_fr)**k * (1 - bet_fr * loss_fr)**(n-k)
+    if log_return:
+        for k in range(n + 1):
+            outcomes += comb(n, k, exact=True) * win_pr**k * (1 - win_pr)**(n-k) * \
+                            np.log((1 + bet_fr * win_fr)**k * (1 - bet_fr * loss_fr)**(n-k))
+    else:
+        for k in range(n + 1):
+            outcomes += comb(n, k, exact=True) * win_pr**k * (1 - win_pr)**(n-k) * \
+                            (1 + bet_fr * win_fr)**k * (1 - bet_fr * loss_fr)**(n-k)
     
     return outcomes
 
 
 if __name__ == "__main__":
-    bet_fr = np.linspace(start=0.0, stop=1.0, num=11, endpoint=True)
-    steps = np.arange(start=1, stop=11, step=1)
-    results = np.zeros((steps.size, bet_fr.size))
+    bet_fr = np.linspace(start=0.0, stop=1.0, num=50, endpoint=False)
+    steps = np.array([1, 2, 3, 5, 10])
+    results_lin = np.zeros((steps.size, bet_fr.size))
+    results_log = np.zeros((steps.size, bet_fr.size))
 
     win_pr = 0.6
     win_fr = 1.0
     loss_fr = 1.0
 
     for idx, n in enumerate(steps):
-        results[idx, :] = bet_n_times_outcome(n, bet_fr, win_pr, win_fr, loss_fr)
+        results_lin[idx, :] = bet_n_times_outcome(n, bet_fr, win_pr, win_fr, loss_fr, log_return=False)
+        results_log[idx, :] = bet_n_times_outcome(n, bet_fr, win_pr, win_fr, loss_fr, log_return=True)
     
     import matplotlib.pyplot as plt
 
-    plt.figure()
-    for idx, _ in enumerate(steps):
-        plt.plot(bet_fr, results[idx, :])    
+    plt.figure(figsize=(11, 5))
+    for idx, n in enumerate(steps):
+        plt.subplot(1, 2, 1)
+        plt.plot(bet_fr, results_lin[idx, :])
+        plt.subplot(1, 2, 2)
+        plt.plot(bet_fr, results_log[idx, :], label=f"n={n}")    
+        # if idx==0 or idx == len(steps) - 1:
+        #     plt.legend(str(n))
+
+    plt.subplot(1, 2, 1)
+    plt.ylim((0.8, 2))
+    plt.ylabel("Linear Returns Expectation")
+    plt.xlabel("Betting Fractiong")
+    plt.subplot(1, 2, 2)
+    plt.ylim((-0.3, 0.3))
+    plt.ylabel("Logarithmic Returns Expectation")
+    plt.xlabel("Betting Fractiong")
+    plt.legend()
     plt.show()
 
