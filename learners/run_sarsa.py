64bea8e5a0dc2e9958bf2b84cb5f9566029c459a
P S Mamkani <psmamkani@protonmail.com>
2022-11-08 08:30:40 -0700

    linear and quadratic sarsa learners

--- learners/run_sarsa.py
+++ learners/run_sarsa.py
@@ -5,18 +5,18 @@ try:    # for intellisense
 except:
     sys.path.append('..')
     from envs import betting_env
-from sarsa import SarsaLearner
+from sarsa import SarsaLearnerQuadratic, SarsaLearnerLinear
 import matplotlib.pyplot as plt
 
-EPISODES = 5000
+bet_env = betting_env.BettingEnvBinary(win_pr=0.6, loss_pr=0.4, win_fr=1.0, loss_fr=1.0, 
+                                        start_cap=10, max_cap=100, min_cap=1, max_steps=20)
 
-if __name__ == '__main__':
-    bet_env = betting_env.BettingEnvBinary(win_pr=0.6, loss_pr=0.4, win_fr=1.0, loss_fr=1.0, 
-                                        start_cap=10, max_cap=1000, min_cap=1, max_steps=20)
+def train_sarsa_quadratic(bet_env):
+    EPISODES = 5000
 
-    sarsa_agent = SarsaLearner(bet_env, learning_rate=1E-9, discount_factor=1.0,
+    sarsa_agent = SarsaLearnerQuadratic(bet_env, learning_rate=1E-9, discount_factor=1.0,
                             epsilon=1.0, epsilon_decay=0.999, epsilon_min=0.05,
-                            q_reg = 1E6, w_reg=10)
+                            q_reg = 1E5, w_reg=10)
     
     state = bet_env.reset()
 
@@ -49,7 +49,49 @@ if __name__ == '__main__':
     # print final states for all episodes
     print(np.array2string(final_states, formatter={'float_kind':lambda x: "%.2f" % x})) 
     print("Weights: ", f"{sarsa_agent.w_01:.2f}, {sarsa_agent.w_10:.2f}, {sarsa_agent.w_02:.2f}, {sarsa_agent.w_11:.2f}, {sarsa_agent.w_20:.2f}")
+    return sarsa_agent
 
+def train_sarsa_linear(bet_env):
+    EPISODES = 5000
+
+    sarsa_agent = SarsaLearnerLinear(bet_env, learning_rate=1E-7, discount_factor=1.0,
+                            epsilon=1.0, epsilon_decay=0.999, epsilon_min=0.01,
+                            q_reg = 1E6, w_reg=10)
+    
+    state = bet_env.reset()
+
+    final_states = np.zeros(EPISODES)
+
+    for e in range(EPISODES):
+        state = bet_env.reset(np.random.uniform(1, 100))
+        states_list = [state]
+        action = sarsa_agent.choose_action(state)
+
+        for i in range(bet_env.max_steps):
+            # print(i, state, action)
+            next_state, next_reward, terminal = bet_env.step(action)
+            states_list.append(next_state)
+
+            if terminal or i == bet_env.max_steps - 1:
+                final_states[e] = next_state
+                sarsa_agent.update_weights_terminal(state, action, next_reward)
+                break
+            else:
+                reward = next_reward
+                next_action = sarsa_agent.choose_action(next_state)
+                sarsa_agent.update_weights(state, action, reward, next_state, next_action)
+                state = next_state
+                action = next_action
+
+        # plot states progression in one episode
+        # print(states_list)
+    
+    # print final states for all episodes
+    print(np.array2string(final_states, formatter={'float_kind':lambda x: "%.2f" % x})) 
+    print("Weights: ", f"{sarsa_agent.w_01:.2f}, {sarsa_agent.w_10:.2f}")
+    return sarsa_agent
+
+def plot_sarsa_action(sarsa_agent, title=None):
     # plot training results:
     s_arr = np.linspace(start=1, stop=100, num=100, endpoint=True)
     a_arr = np.zeros(len(s_arr))
@@ -58,6 +100,13 @@ if __name__ == '__main__':
     
     plt.figure()
     plt.plot(s_arr, a_arr)
-    plt.ylim((-5, 100))
+    plt.ylim((-5, s_arr[-1] * 1.05))
+    if title is not None: plt.title(title)
     plt.show()
+
+agent_quadratic = train_sarsa_quadratic(bet_env)
+plot_sarsa_action(agent_quadratic, title="quadratic sarsa")
+
+agent_linear = train_sarsa_linear(bet_env)
+plot_sarsa_action(agent_linear, title="linear sarsa")
 
--- learners/sarsa.py
+++ learners/sarsa.py
@@ -31,6 +31,7 @@
 # 
 # To allow exploration, epsilon-greedy action selection is used.
 # 
+# The second class has a simpler linear form for q(s, a, w) =  w_01 * a + w_10 * s
 
 import numpy as np
 import sys
@@ -40,8 +41,11 @@ except:
     sys.path.append('..')
     from envs import betting_env
 
-class SarsaLearner:
-    def __init__(self, env, learning_rate=0.1, discount_factor=1.0,
+class SarsaLearnerQuadratic:
+    '''Quadratic Implementation of Semigradient Sarsa where q is approximated using a second order polynomial in 
+    s and a: 
+    q(s, a, w) = w_01 * a + w_10 * s + w_02 * a ^ 2 + w_11 * s * a + w_20 * s^2'''
+    def __init__(self, env, learning_rate=1E-8, discount_factor=1.0,
                     epsilon=0.1, epsilon_decay=0.99, epsilon_min=0.001,
                     q_reg=1E6, w_reg=1E3):
         self.env = env
@@ -66,10 +70,7 @@ class SarsaLearner:
         if np.isnan(q) or np.isinf(q):
             print("overflow problem in calculating q:")
             print(f"w_01: {self.w_01}    w_10: {self.w_10}    w_02: {self.w_20}    w_11: {self.w_11}    w_20: {self.w_20} ")
-        
-        # q regularization: we don't want q to grow too much
-        if q > 1E6:     q = 1E6
-        if q < -1E6:    q = -1E6
+        q = self._regularize_q(q)
         return q
 
     def choose_action(self, state):
@@ -125,43 +126,83 @@ class SarsaLearner:
     def _regularize_q(self, q_in):
         return max(min(q_in, self.q_reg), -self.q_reg)
         
-
     def _adjust_epsilon(self):
         if self.eps > self.eps_min: self.eps *= self.eps_dec
         
-    
-# EPISODES = 100
-# if __name__ == '__main__':
-#     bet_env = betting_env.BettingEnvBinary(win_pr=0.6, loss_pr=0.4, win_fr=1.0, loss_fr=1.0, 
-#                                         start_cap=100, max_cap=1E6, min_cap=1, max_steps=20)
 
-#     sarsa_agent = SarsaLearner(bet_env, learning_rate=0.1, discount_factor=1.0,
-#                             epsilon=0.1, epsilon_decay=0.99, epsilon_min=0.001)
-    
-#     state = bet_env.reset()
+class SarsaLearnerLinear:
+    '''q(s, a, w) is approximated as a linear function of s and a: q(s, a, w) = w_01 * a + w_10 * s'''
+    def __init__(self, env, learning_rate=0.1, discount_factor=1.0,
+                    epsilon=0.1, epsilon_decay=0.99, epsilon_min=0.001,
+                    q_reg=1E6, w_reg=1E3):
+        self.env = env
+        self.lr = learning_rate
+        self.gamma = discount_factor
+        self.eps = epsilon
+        self.eps_dec = epsilon_decay
+        self.eps_min = epsilon_min
+        self.q_reg = q_reg
+        self.w_reg = w_reg
 
-#     final_states = np.zeros(EPISODES)
+        self._initialize_weights()
 
-#     for e in range(EPISODES):
-#         states = np.zeros(bet_env.max_steps)
-#         state = bet_env.reset()
-#         action = sarsa_agent.choose_action(state)
+    def _initialize_weights(self):
+        self.w_01, self.w_10 = [0, 0]
 
-#         for i in range(bet_env.max_steps):
-#             next_state, next_reward, terminal = bet_env.step(action)
-#             states.append(next_state)
+    def _q_val(self, s, a):
+        # quadratic form in state (s) and action (a)
+        q = self.w_01 * a + self.w_10 * s
+        if np.isnan(q) or np.isinf(q):
+            print("overflow problem in calculating q:")
+            print(f"w_01: {self.w_01}    w_10: {self.w_10}")
+        
+        q = self._regularize_q(q)
+        return q
 
-#             if terminal or i == bet_env.max_steps - 1:
-#                 final_states[e] = state
-#                 sarsa_agent.update_weights_terminal(state, action, reward)
-#                 break
-#             else:
-#                 reward = next_reward
-#                 next_action = sarsa_agent.choose_action(state)
-#                 sarsa_agent.update_weights(state, action, reward, next_state, next_action)
-#                 state = next_state
-#                 action = next_action
+    def choose_action(self, state):
+        # epsilon-greedy
+        if np.random.uniform(0, 1) <= self.eps:
+            action = np.random.uniform(0, state)
+        else:
+            action = self._optimal_action(state)
+        
+        # action (betting amount) must be between 0 and full capital (state):
+        if action < 0: action = 0
+        if action > state: action = state
+        
+        return action
 
-#         # plot states progression in one episode
-    
-#     # plot final states for all episodes
+    def _optimal_action(self, state):
+        # for a linear function, we only need to compare the two extremes of a=0 and a=s. i.e. binary action
+        q_a_0 = self._q_val(state, 0)   #       q(s, 0) for a = 0, i.e. betting 0
+        q_a_s = self._q_val(state, state)   #   q(s, s) for acion = state, i.e. betting full
+
+        return max([(0, q_a_0), (state, q_a_s)], key=lambda t: t[1])[0]  # 
+        
+    def update_weights_terminal(self, s, a, reward):
+        # terminal state update
+        # w → w + alpha * [R_(t+1) - q(s(t), a(t), w(t))] * Grad_w(q(s, a, w))
+        # Grad_w(q) = [a, s, a^2, s.a, s^2]
+        cor = self.lr * (reward - self._q_val(s, a))
+        [self.w_01, self.w_10] =  [self.w_01, self.w_10] + cor * np.array([a, s])
+        self._regularize_weights()
+        self._adjust_epsilon()
+
+    def update_weights(self, s, a, reward, next_s, next_a):
+        # non-terminal update
+        # w → w + alpha * [R_(t+1) + gamma * q(s(t+1), a(t+1), w(t)) - q(s(t), a(t), w(t))] * Grad_w
+        cor = self.lr * (reward + self.gamma * self._q_val(next_s, next_a) - self._q_val(s, a))
+        [self.w_01, self.w_10] = [self.w_01, self.w_10] + cor * np.array([a, s])
+        
+        self._regularize_weights()
+        self._adjust_epsilon()
+
+    def _regularize_weights(self):
+        # arr = [self.w_01, self.w_10]
+        [self.w_01, self.w_10] = list( map(lambda x: max(min(x, self.w_reg), -self.w_reg), [self.w_01, self.w_10]) )
+
+    def _regularize_q(self, q_in):
+        return max(min(q_in, self.q_reg), -self.q_reg)
+        
+    def _adjust_epsilon(self):
+        if self.eps > self.eps_min: self.eps *= self.eps_dec
