9db45a75bab9ffc4906ff92aeac6b5f138a921c3
P S Mamkani <psmamkani@protonmail.com>
2022-12-26 11:49:32 -0700

    utility function normalization, plot cleanup

--- learners/nn_q_learn_memory.py
+++ learners/nn_q_learn_memory.py
@@ -194,7 +194,7 @@ class QInvestAgent:
                     pred = self.model(self.x_valid)[:, 0]
                     loss_v = self.loss_fn(pred, self.y_valid.squeeze())
                     self.valid_loss_hist[epis // epis_prog] = loss_v.item() # / self.y_valid.size()[0]
-                print(f"Episode: {epis} | Training Loss: {self.train_loss_hist[epis // epis_prog]} | Validation Loss: {self.valid_loss_hist[epis // epis_prog]}")
+                print(f"Episode: {epis:{3}} | Training Loss: {self.train_loss_hist[epis // epis_prog]:{9}.{6}} | Validation Loss: {self.valid_loss_hist[epis // epis_prog]:{9}.{6}}")
         return self.train_loss_hist, self.valid_loss_hist
 
     def generate_validation_data(self, prob_arr, outcome_arr, util_func, st_range=[0.0, 1.0],  ac_range=[0, 0.99], num_st=20, num_ac=20):
@@ -210,25 +210,18 @@ class QInvestAgent:
         self.num_st_val, self.num_ac_val = num_st, num_ac
         return self.x_valid, self.y_valid
 
-    @classmethod
-    def get_determ_reward(cls, x_in, prob_arr, outcome_arr, util_func):
-        y = np.zeros(len(x_in))
-        p_arr, o_arr = np.array(prob_arr), np.array(outcome_arr)
-        for idx, (s, a) in enumerate(x_in):
-            # print((1-x) * np.ones(len(o_arr)) + x * o_arr)
-            #                                    ↓ amount not risked ↓   +   ↓ amount risked ↓  -  ↓ util before bet ↓
-            y[idx] = np.sum(p_arr * util_func(s * (1-a) * np.ones(len(o_arr)) + s * a * o_arr) - \
-                            p_arr * util_func(s * np.ones(len(o_arr))))
-        return y
-
     def plot_training_history(self):
-        plt.figure(figsize=(7, 5))
+        plt.figure(figsize=(10, 5))
+        plt.subplot(1, 2, 1)
         plt.plot(np.arange(len(self.train_loss_hist)) + 1, self.train_loss_hist)
-        plt.plot(np.arange(len(self.valid_loss_hist)) + 1, self.valid_loss_hist)
-        plt.title(f"Q-NN Performance History\nMemory: {self.memory.maxlen}")
+        plt.title(f"Q-NN Performance History: Training\nMemory: {self.memory.maxlen}")
         plt.xlabel("Episodes")
         plt.ylabel("Loss")
-        plt.legend(["Training", "Validation"])
+        plt.subplot(1, 2, 2) 
+        plt.plot(np.arange(len(self.valid_loss_hist)) + 1, self.valid_loss_hist)
+        plt.title(f"Q-NN Performance History: Validation")
+        plt.xlabel("Episodes")
+        plt.ylabel("Loss")
         plt.show()
 
     def plot_performance(self, show_legends=True, num_st=10, num_ac=10):
@@ -247,16 +240,16 @@ class QInvestAgent:
         # plt.plot(x_valid[-num_ac:, 1], y_valid[-num_ac:])
         # print(x_valid[0:self.num_st_val * self.num_ac_val:self.num_ac_val, 0].squeeze())
 
-        legends = ['_no_label'] * self.num_st_val  # 
+        legends = [False] * self.num_st_val  # 
         if show_legends: legends[0], legends[-1] = f"s = {x_valid[0, 0]}", f"s = {x_valid[-1, 0]}"
         # print(legends)
 
         for s_idx in range(self.num_st_val):
             idx = s_idx * self.num_ac_val
-            plt.plot(x_valid[idx:idx + self.num_ac_val, 1], y_valid[idx:idx + self.num_ac_val])
+            plt.plot(x_valid[idx:idx + self.num_ac_val, 1], y_valid[idx:idx + self.num_ac_val], label=legends[s_idx])
         plt.title("Validation Data")
         plt.xlabel("Investment Fraction")
-        plt.legend(legends)
+        # plt.legend(legends)
         # if show_legends:
         #     plt.legend(x_valid[0:self.num_st_val * self.num_ac_val: self.num_ac_val, 0])   # labels/legends for different s value
 
@@ -264,11 +257,11 @@ class QInvestAgent:
         y_pred  = y_pred.detach().numpy()
         for s_idx in range(self.num_st_val):
             idx = s_idx * self.num_ac_val
-            plt.plot(x_valid[idx:idx + self.num_ac_val, 1], y_pred[idx:idx + self.num_ac_val])
+            plt.plot(x_valid[idx:idx + self.num_ac_val, 1], y_pred[idx:idx + self.num_ac_val], label=legends[s_idx])
             # plt.plot(x_valid[0:num_ac, 1], y_pred[idx:idx + num_ac])
         plt.title(f"Model Performance\nLearning Rate: {lr}")
         plt.xlabel("Input X to Model: Investment Fraction")
-        plt.legend(legends)
+        # plt.legend(legends)
         # if show_legends:
         #     plt.legend(x_valid[0:self.num_st_val * self.num_ac_val: self.num_ac_val, 0])   # labels/legends for different s values
         plt.show()
@@ -278,16 +271,38 @@ class QInvestAgent:
         
         plt.figure(figsize=(12, 5))
         plt.subplot(1, 2, 1)
-        plt.contourf(y_2d_test)
+        plt.contourf(y_2d_test, levels=20)
         plt.colorbar()
         plt.title(f"Target Contour")
-        # plt.xlabel("Input X to Model")
+        plt.xlabel("Action: Investment Fraction")
+        plt.ylabel("State: Capital")
         # plt.legend(["Model", "Theory"])
         plt.subplot(1, 2, 2)
-        plt.contourf(y_2d_pred)
+        plt.contourf(y_2d_pred, levels=20)
         plt.colorbar()
         plt.title(f"Predicted Contour")
         plt.show()
+    
+    @classmethod
+    def normalize_util_func(cls, u_func, minmax=[0, 1], num=101, verbose=False):
+        st_arr = np.linspace(minmax[0], minmax[1], num, endpoint=True)
+        util_arr = u_func(st_arr)
+        uf_max, uf_min = max(util_arr), min(util_arr)
+        diff, avg = uf_max - uf_min, (uf_max + uf_min) / 2.0
+        normalized_uf = lambda x: (u_func(x) - avg) / diff
+        if verbose: print(f"avg: {avg} | diff: {diff}")
+        return normalized_uf
+
+    @classmethod
+    def get_determ_reward(cls, x_in, prob_arr, outcome_arr, util_func):
+        y = np.zeros(len(x_in))
+        p_arr, o_arr = np.array(prob_arr), np.array(outcome_arr)
+        for idx, (s, a) in enumerate(x_in):
+            # print((1-x) * np.ones(len(o_arr)) + x * o_arr)
+            #                                    ↓ amount not risked ↓   +   ↓ amount risked ↓  -  ↓ util before bet ↓
+            y[idx] = np.sum(p_arr * util_func(s * (1-a) * np.ones(len(o_arr)) + s * a * o_arr) - \
+                            p_arr * util_func(s * np.ones(len(o_arr))))
+        return y
     
 if __name__ == '__main__':
     rng = np.random.default_rng()
@@ -299,9 +314,14 @@ if __name__ == '__main__':
     # validation data grid
     st_range = np.array([0.0, 1.0])
     ac_range = np.array([0, 1.0])
-    st_minmax = np.array([0.01, 5.0])
-    # util_func = lambda x: log_util(x, x_reg=1E-5)
-    util_func = lambda x: x
+    st_minmax = np.array([0.0, 5.0])
+    util_func = lambda x: log_util(x, x_reg=1E-3)
+    # util_func = lambda x: x
+    normalize_uf = False     # True: Normalize utility function to [-1 +1] range | False: use raw util func
+    if normalize_uf: 
+        n_util_func = QInvestAgent.normalize_util_func(util_func, minmax=st_minmax, num=101, verbose=True)
+    else:
+        n_util_func = util_func
     mem_size = int(2E3)
     recall_mech = 'recent' # 'recent' | 'random'
     lr = 1E-5
@@ -311,7 +331,7 @@ if __name__ == '__main__':
     gamma = 0.0
     layers_sz = [10, 10]
     next_step_lookup = False    # True: q system | False: the simplest case, no looking up the next step (same as gamma=0)
-    epochs_per_episode = 10      # number of cycles of training in self.learn_step per episode/call
+    epochs_per_episode = 100      # number of cycles of training in self.learn_step per episode/call
 
     num_epis = 30_000 // epochs_per_episode
     epis_prog = 1000 // epochs_per_episode
